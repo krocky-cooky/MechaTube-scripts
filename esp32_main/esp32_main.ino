cf2129aa336f712255ab7d3586e794a544c5548b
thgcMtdh <kyouho315@gmail.com>
2021-10-04 23:15:29 +0900

    fix can send code

--- esp32_main/esp32_main.ino
+++ esp32_main/esp32_main.ino
@@ -1,7 +1,6 @@
-// Copyright (c) Sandeep Mistry. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
+#include <Arduino.h>
 #include <CAN.h>
+
 #define MOTOR_ID 64
 #define DRIVER_ID 0
 
@@ -27,11 +26,9 @@ void loop() {
   delay(1000);
 
   // enter the motor control mode
-  Serial.print("Sending packet ... ");
-  CAN.beginPacket(MOTOR_ID);
-  uint8_t msgToEnter[8] = {0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xfc};
-  CAN.write(msgToEnter, 8);
-  CAN.endPacket();
+  Serial.print("Sending packet to enter motor control mode... ");
+  uint8_t msg[8] = {0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xfc};
+  sendCan(msg, 8);
   Serial.println("done");
 
   delay(1000);
@@ -39,14 +36,26 @@ void loop() {
   // constant speed 
   while(1) {
     Serial.print("Sending packet ... ");
-    CAN.beginPacket(MOTOR_ID);
-    uint8_t msgToEnter[8] = {0x7f, 0xff, 0x84, 0x30, 0x0, 0x33, 0x37, 0xff};
-    CAN.write(msgToEnter, 8);
-    CAN.endPacket();
+    uint8_t msg[8] = {0x7f, 0xff, 0x84, 0x30, 0x0, 0x33, 0x37, 0xff};
+    sendCan(msg, 8);
     Serial.println("done");
     delay(1000);
   }
 
   delay(1000);
   
+}
+
+/// @brief CAN送信を行う
+/// @param[in] data 送りたいデータ配列
+/// @param[in] length 送信するバイト数(上限は8byte)
+/// @return 0:fail, 1:success
+int sendCan(uint8_t* data, int length) {
+  if (length < 1 || length > 8) return 0;
+
+  if (!CAN.beginPacket(MOTOR_ID)) return 0;
+  if (!CAN.write(data, length)) return 0;
+  if (!CAN.endPacket()) return 0;
+
+  return 1;
 }
