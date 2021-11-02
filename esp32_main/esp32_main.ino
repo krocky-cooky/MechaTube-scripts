4c4f22214ef4ea9d4fcec20486201c411750abc8
Tomoki-japan <tomoki.kurikuri.1122@gmail.com>
2021-11-02 14:36:55 +0900

    最大トルクを10Nmに戻した

--- esp32_main/esp32_main.ino
+++ esp32_main/esp32_main.ino
@@ -13,7 +13,7 @@
 //閾値等
 #define FORCE_THRESHOLD_OF_HANDSWICH 10.0                             //手元スイッチのオンオフを識別するための、スイッチにかかる力の閾値 [N]
 #define THRESHOLD_OF_MOTOR_SPEED_FOR_DETERMINING_ECCENTRIC_MOTION 0.5 //エキセン動作を判定するための、モータの回転速度の閾値 [rad/s]
-#define MAX_TORQUE 20.0                                               //許容する最大トルク [Nm]
+#define MAX_TORQUE 10.0                                               //許容する最大トルク [Nm]
 #define MAX_SPEED 10.0                                       //許容する最大回転速さ [rad/s]
 
 
