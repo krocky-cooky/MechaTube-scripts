6ae6c215736dd446681fb642cdc12deb39a83cb1
thgcMtdh <kyouho315@gmail.com>
2022-08-27 18:07:00 +0900

    upd: 受信側コードをライブラリ化し、単体での正常動作確認済

---
+++ tension_meter_machine/BTTensionMeter.hpp
@@ -0,0 +1,61 @@
+/**
+ * @file BTTensionMeter.hpp
+ * @author Hibiki Matsuda
+ * @brief Bluetooth張力計(tension_meter.ino)からデータを受信するライブラリ
+ * @date 2022-08-27
+ */
+#include "BluetoothSerial.h"
+
+/*
+ * 想定している受信メッセージの形式
+ *  index       0  1  2  3  4  5  6         7   8
+ *  content  0x02  1  6  5  0  1  ,         3  \n
+ *  meaning  STX   Tension [gram]    checksum  LF
+ *                 -> 16501g         1+6+5+0+1 end
+ */
+
+class BTTensionMeter
+{
+private:
+  static const size_t BT_MSG_BUFSIZE_; // Bluetooth受信メッセージをコピーするバッファのサイズ
+  static const unsigned long TIMEOUT_; // 直近のメッセージ到着からこの時間[ms]だけ経過しても次のメッセージが来ないとき接続断とみなす
+  BluetoothSerial &SerialBT_;
+  unsigned long lastReceivedMillis_; // 最後に受信した時刻[ms]
+  bool available_;                   // 最新値到着フラグ。受信した値がまだgetTension()により読まれていないときtrueになる
+  int tension_;                      // 受信した張力の最新値[g]
+
+  int calc_checksum(int val);
+
+public:
+  BTTensionMeter(BluetoothSerial &serialBT);
+
+  /**
+   * @brief Initialize library
+   */
+  void begin();
+
+  /**
+   * @brief Receive messages from meter. Must call this function in mainloop
+   */
+  void loop();
+
+  /**
+   * @brief Return whether the tension meter is conncted or not.
+   * @return true Tension meter is connected.
+   * @return false Tension meter is not connected (message hasn't arrived for 50ms or more).
+   */
+  bool connected();
+
+  /**
+   * @brief Return true when a new value has been arrived.
+   * @return true when a new value has been arrived but not read by user.
+   * @return false when the latest value has also been read. You can still use getTension() to obtain the latest value.
+   */
+  bool available();
+
+  /**
+   * @brief Get the latest tension sent from meter.
+   * @return (int) Measured tension [g]
+   */
+  int getTension();
+};
--- tension_meter_machine/tension_meter_machine.ino
+++ tension_meter_machine/tension_meter_machine.ino
@@ -1,15 +1,8 @@
+#include "BTTensionMeter.hpp"
 #include "BluetoothSerial.h"
 
 BluetoothSerial SerialBT;
-constexpr size_t BT_MSG_BUFSIZE = 32; // Bluetooth受信メッセージのバッファ長
-
-/*
- * 想定している受信メッセージの形式
- *  index       0  1  2  3  4  5  6         7   8
- *  content  0x02  1  6  5  0  1  ,         3  \n
- *  meaning  STX   Tension [gram]    checksum  LF
- *                 -> 16501g         1+6+5+0+1 end
- */
+BTTensionMeter tensionMeter(SerialBT);
 
 void setup()
 {
@@ -17,53 +10,13 @@ void setup()
   Serial.println("Serial begin");
   SerialBT.begin("Machine-ESP32"); // Bluetooth device name
   Serial.println("SerialBT begin");
+  tensionMeter.begin();
 }
 
 void loop()
 {
-  if (SerialBT.available()) {
-    if (SerialBT.read() == 0x02) { // 開始コード STX で始まる場合、有効なメッセージなので読み込む
-
-      char msg[BT_MSG_BUFSIZE];          // 受信メッセージをコピーするバッファ
-      memset(msg, 0x00, BT_MSG_BUFSIZE); // ゼロ埋め
-      SerialBT.readBytesUntil('\n', msg, BT_MSG_BUFSIZE);
-
-      int val = 0;
-      int checksum = 0;
-      sscanf(msg, "%d,%d", &val, &checksum); // 受信メッセージから張力とチェックサムを読み取る
-
-      int mychecksum = calc_checksum(val); // valからチェックサムを計算
-      if (mychecksum == checksum) {        // チェックサム照合
-        Serial.printf("%d,%d\n", val, checksum);
-      } else {
-        Serial.printf("checksum failure!\n");
-      }
-
-    } else { // 開始コード以外を検出した場合、捨てる
-      while (SerialBT.available()) {
-        SerialBT.read();
-      }
-    }
+  tensionMeter.loop();
+  if (tensionMeter.available()) {
+    Serial.println(tensionMeter.getTension());
   }
-}
-
-/**
- * @brief 数値を10進数でprintしたときの各桁の和を計算し、下1桁を返す
- * @param val 数値 (例: 16501）
- * @return 各桁の和（例: 3）
- */
-int calc_checksum(int val)
-{
-  constexpr size_t INT_MAX_DIGIT = 12; // intは最長11文字(-2147483648) + null文字
-  char val_str[INT_MAX_DIGIT];
-  memset(val_str, 0x00, INT_MAX_DIGIT); // ゼロ埋めする
-  sprintf(val_str, "%d", val);          // valを10進数の文字列としてval_strにprint
-  int sum = 0;
-  for (size_t i = 0; i < INT_MAX_DIGIT; i++) {
-    if (val_str[i] < '0' || val_str[i] > '9') { // '0'-'9'以外の文字の場合、'0'に置き換える
-      val_str[i] = '0';
-    }
-    sum += static_cast<int>(val_str[i] - '0'); // 和を計算
-  }
-  return (sum - (sum / 10) * 10); // sum の1桁目を返す
 }
