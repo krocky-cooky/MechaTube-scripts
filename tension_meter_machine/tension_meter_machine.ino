2710a2b40eb67fc62ad3bd27aac0217dc1b3ed6f
thgcMtdh <kyouho315@gmail.com>
2022-09-11 14:34:31 +0900

    upd: テキスト形式で値を飛ばすことに成功

--- tension_meter_machine/tension_meter_machine.ino
+++ tension_meter_machine/tension_meter_machine.ino
@@ -7,13 +7,16 @@ const BLEUUID TENSIONMETER_CHARACTERISTIC_UUID((uint16_t)0x2A98); // Weight (定
 
 const char MACHINE_DEVICE_NAME[] = "ESP32-Machine";  // セントラルに表示されるマシンのデバイス名
 const BLEUUID MACHINE_SERVICE_UUID("91bad492-b950-4226-aa2b-4ede9fa42f59");  // マシンがサーバとして振舞う際のService UUID
-const BLEUUID MACHINE_CHARACTERISTIC_UUID("f78ebbff-c8b7-4107-93de-889a6a06d408");  // マシンがサーバとして振舞う際のCharacteristic UUID
+const BLEUUID MACHINE_LOG_CHARACTERISTIC_UUID("f78ebbff-c8b7-4107-93de-889a6a06d408");  // マシンがサーバとして振舞う際の、ログ送信Characteristic UUID
+const BLEUUID MACHINE_COMMAND_CHARACTERISTIC_UUID("f78ebbff-c8b7-4107-93de-889a6a06d409");  // マシンがサーバとして振舞う際の、指令受信Characteristic UUID
 
-bool tensionMeterFound = false; // Scanで張力計を発見したらtrueとなるフラグ
-BLEAddress tensionMeterAddress("00:00:00:00:00:00"); // Scanで張力計を発見したら張力計のアドレスが歳入される
 BLEClient* pBleClient = NULL;
 BLERemoteService* pBleRemoteService = NULL;
 BLERemoteCharacteristic* pBleRemoteCharacteristic = NULL;
+bool tensionMeterFound = false; // Scanで張力計を発見したらtrueとなるフラグ
+BLEAddress tensionMeterAddress("00:00:00:00:00:00"); // Scanで張力計を発見したら張力計のアドレスが歳入される
+int tension = 0;  // 受信した張力[mgf]
+unsigned long lastReceived = 0;  // 最後に量力を受信した時刻[ms]。一定時間以上受信できなければfalseに戻す
 
 BLEServer* pServer = NULL;
 BLEService* pService = NULL;
@@ -33,7 +36,19 @@ class FoundCallbacks : public BLEAdvertisedDeviceCallbacks
 };
 
 void tensionMeterNotifyCallback(BLERemoteCharacteristic* pBLERemoteCharacteristic, uint8_t* data, size_t length, bool isNotify) {
-  Serial.println(*reinterpret_cast<int*>(data));
+  tension = *((int*)data); // 受信したデータから張力を取得
+  lastReceived = millis(); // 受信時刻を記録
+}
+
+void sendText(String str)
+{
+  pCharacteristic->setValue((uint8_t*)(str.c_str()), str.length());
+  pCharacteristic->notify();
+}
+void sendText(const char* str)
+{
+  pCharacteristic->setValue((uint8_t*)str, strlen(str));
+  pCharacteristic->notify();
 }
 
 void setup()
@@ -70,7 +85,7 @@ void setup()
   // pServer->setCallbacks(new FuncServerCallbacks());
   pService = pServer->createService(MACHINE_SERVICE_UUID);
   Serial.println("setup 2");
-  pCharacteristic = pService->createCharacteristic(MACHINE_CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_NOTIFY);
+  pCharacteristic = pService->createCharacteristic(MACHINE_LOG_CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_NOTIFY);
   Serial.println("setup 3");
   pService->start();
   Serial.println("setup 4");
@@ -89,5 +104,8 @@ void setup()
 void loop()
 {
   Serial.println("loop");
+  char buf[64];
+  sprintf(buf, "tension: %d", tension);
+  sendText(buf);
   delay(1000);
 }
